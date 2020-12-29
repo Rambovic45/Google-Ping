2a682135d0de25d3f0421f33d8c900a2561db297
Name.bat <58370115+Rambovic45@users.noreply.github.com>
2020-12-29 12:02:41 -0800

    First Commit

---
+++ Google Ping.ps1
@@ -0,0 +1,98 @@
+﻿
+# If you see this you have not run a file correctly for a run a file correctly : 
+# Right click in the file
+# Click in the button 'Run whit PowerShell'. If your windows is a other language click in a correcly buttun
+# Joy
+
+# If you go down you see a code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Cmd Code
+
+
+
+
+
+# Var
+Set-Alias -Name ping -Value Test-NetConnection
+Set-Alias -Name pin -Value Test-NetConnection
+Set-Alias -Name pi -Value Test-NetConnection
+Set-Alias -Name p -Value Test-NetConnection
+Set-Alias -Name png -Value Test-NetConnection
+Set-Alias -Name pig -Value Test-NetConnection
+Set-Alias -Name pg -Value Test-NetConnection
+
+# Ping Message
+Write-Output 'Ping of Google'
+
+# Pingin
+
+# Dectec-Bug-IP ->("google.com") = IP
+# Dectec-Ip ->("localhost") = CumputerIp
+# Connect(IP)
+# Offline(yes)
+# Del(google-security)
+# Finished(All)
+# Del(Google-Hist, CumputerIp)
+
+# Result
+ping 172.217.4.206
+
+# Pause
+pause
